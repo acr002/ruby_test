11693ae328c3e6941b5504e7283157bef1ba31b2
acr002 <tamada@across-net.co.jp>
2021-04-16 16:34:40 +0900

    Search git

--- 01 hash/run.rb
+++ 01 hash/run.rb
@@ -1,10 +1,16 @@
 
+def test(**h)
+  a = []
+  h.each do |k, v|
+    a << "#{k}: #{v}"
+  end
+  a.join(', ')
+end
+
 h = {a: 1, b: 2, c: 'test'}
 p h
 
-h.each do |k, v|
-  puts "#{k}: #{v}"
-end
-
-a = 3
+a = test(**h)
+p a
+################################
 
