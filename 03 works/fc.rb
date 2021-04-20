5bad0fd5f977ad7f2753eb5babb627438cfd0798
acr002 <tamada@across-net.co.jp>
2021-04-20 16:32:22 +0900

    Emit

--- 03 works/fc.rb
+++ 03 works/fc.rb
@@ -1,5 +1,7 @@
+# coding: cp932
 
-require 'pry-byebug'
+# require 'pry-byebug'
+require 'H:\A\ruby\00 lib/log.rb'
 
 class FC
   attr_reader :basename
@@ -11,10 +13,27 @@ class FC
 end
 
 fc = FC.new
-binding.pry
-p fc
-p fc.basename
+# log = Log.new('test')
+# # binding.pry
+# log.sf fc
+# # p fc
+# # p fc.basename
+# 9.times do |i|
+#   log.sf "test #{i + 1}"
+# end
+# log.sf 'Ç≈Ç´ÇÈÇ©Ç»ÅH', 9, 3, 'test'
+# log.put_csv
 
+log = {}
+log[:a] = Log.new('a')
+log[:b] = Log.new('b')
+log[:c] = Log.new('c')
+log[:a].sf 'log test'
+log[:b].sf fc
+9.times do |i|
+  log[:c].sf "test #{i + 1}"
+end
+log.log_put_csv
 
 
 
--- 03 works/run.rb
+++ 03 works/run.rb
@@ -1,22 +1,4 @@
-# coding: cp932
 
-# require 'pry-byebug'
-require 'pry'
-
-# 2021.04.20(火)
-# lineを加工していき、結果を出力する必要があります。
-# 元のlineを加工していくgc.map!でもいいですし、新しい配列に挿入していくやり方でもいいと思います。
-# result = gc.map{} でもいいと思います。
-a = %w(q b c d e f g a)
-binding.pry
-b = a.map do |e|
-  unless e == 'e'
-    'test'
-  else
-    e
-  end
-end
-p a, b
 ################################
 
 
