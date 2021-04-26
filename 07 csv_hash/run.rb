2be2d4c811a31936d50688115c8021bcb8def0ff
acr002 <tamada@across-net.co.jp>
2021-04-26 16:31:35 +0900

    Fix argument

--- 07 csv_hash/run.rb
+++ 07 csv_hash/run.rb
@@ -6,11 +6,17 @@ require 'json'
 # hashからjsonにします。
 # windowsで使われる事を想定しているので、encodingをcp932に指定しています。
 
-def csv_hash(path)
-  a = CSV.read(path, encoding: 'cp932:utf-8')
+# 引数 path:    ファイル名
+# 引数 x_key:   keyが入っている列番号(1から始めます)
+# 引数 x_value: valueが入っている列番号(1から始めます)
+# hashのkeyはシンボル化しています。
+def csv_hash(path, x_key = 1, x_value = 2)
   h = Hash.new
+  xk = x_key - 1
+  xv = x_value - 1
+  a = CSV.read(path, encoding: 'cp932:utf-8')
   a.each do |row|
-    h[row[0].to_sym] = row[1]
+    h[row[xk].to_sym] = row[xv]
   end
   h
 end
@@ -22,6 +28,7 @@ def put_json_pg(path, h)
 end
 
 h = csv_hash('sample.csv')
+p h
 put_json_pg('result.json', h)
 # puts JSON.pretty_generate(h)
 
