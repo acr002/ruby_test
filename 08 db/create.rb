10440dd1d1315d5af1248da0fc2524b43d3951a5
acr002 <tamada@across-net.co.jp>
2021-05-14 16:32:41 +0900

    Add create

--- 08 db/create.rb
+++ 08 db/create.rb
@@ -1,17 +1,20 @@
 
+require 'csv'
 require 'sqlite3'
 
+sf = CSV.table('RDmcs3.csv')
 db = SQLite3::Database.new('test.db')
-
-sql = 'create table users (id integer primary key, name text);'
+sql = "create table rd (#{sf.headers.map{|e| "#{e} integer"}.join(',')});"
 db.execute(sql)
 
-db.execute('insert into users (name) values (?)', 'maruvi')
-db.execute('insert into users (name) values (?)', 'sfc')
-
-db.execute('select * from users') do |row|
-  p row
+db.transaction do
+  sf.each do |row|
+    sf.headers.each do |e|
+      sql = "insert into rd (#{e}) values (?)"
+      db.execute(sql, row[e])
+    end
+  end
 end
 
-
+db.close
 
--- 08 db/run.rb
+++ 08 db/run.rb
@@ -1,2 +1,10 @@
 
+require 'sqlite3'
+
+SQLite3::Database.new('test.db') do |db|
+  gc = db.execute('select * from rd')
+  gc.each do |e|
+    p e
+  end
+end
 
