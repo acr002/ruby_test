7645c4f5af0553793723a123165b0a0a5ec97384
acr002 <tamada@across-net.co.jp>
2021-05-28 11:05:08 +0900

    Add comments

--- 07 csv_hash/csv_hash_ver4.rb
+++ 07 csv_hash/csv_hash_ver4.rb
@@ -1,6 +1,13 @@
 
 require 'csv'
 
+# csvのデータの内、２つの列をHashに格納します。
+# エクセルのvlookup()のテーブルのようなイメージです。
+# 例えば「A列をキーにして、B列の値を使う」感じです。
+# 
+# 本来ならば、csvのデータがある程度正しいかどうかを確認すべきです。
+# keyとなる列に重複はないかとか、keyは数値なのか文字列かなのかなどです。
+
 def csv_hash(path, x_key = 2, x_value = 3)
   a = CSV.read(path, encoding: 'cp932:utf-8', headers: true)
   xk = a.headers[x_key - 1]
@@ -12,5 +19,7 @@ end
 h = csv_hash('sample.csv', 7, 8)
 # h = csv_hash('sample.csv')
 p h
+
+
 
 
