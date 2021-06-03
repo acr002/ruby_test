32b150bd36abc15afa4b2c8f995b0547dd49acf3
acr002 <tamada@across-net.co.jp>
2021-06-03 11:11:04 +0900

    Add sample

--- 04 test/17 send/run.rb
+++ 04 test/17 send/run.rb
@@ -1,10 +1,22 @@
 
-class TestSEND
-  def 
+class Foo
+  def foo() 'foo' end
+
+  def bar()
+    p 'called bar'
+    'bar'
+  end
+
+  def baz() 'baz' end
+end
+
+methods = {a: :foo, b: :bar, c: :baz}
+methods.each do |k, v|
+  p Foo.new.send(v)
 end
 
 
 
-
-     Object#public_send, BasicObject#__send__, Object#method, Kernel.#eval, Proc, Method
+__END__
+Object#public_send, BasicObject#__send__, Object#method, Kernel.#eval, Proc, Method
 
