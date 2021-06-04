c9a8f9aca28ceb33f837b97482297677ad5ca40d
acr002 <tamada@across-net.co.jp>
2021-06-04 15:32:23 +0900

    Test split

--- 04 test/16 regexp/run.rb
+++ 04 test/16 regexp/run.rb
@@ -4,40 +4,5 @@ hr = '-' * 36
 
 
 
-# a = 'You say, "Yes, I am.", "No, I am not.".'
-a = 'You say.(Yes. I am.) . (No. I am not.).'
-pat1 = /"([^"]*)"[^"]*"([^"]*)"/
-# pat1 = /"(A)"A"(A)"/
-pat2 = /"(.*?)"[^"]*"(.*?)"/
-pat1_1 = /"([^"]*)"/
-pat2_1 = /"(.*?)"/
-# pat = /,(?=(?![^"]*"[^"]*")*[^"]*$)/
-#     /,(?=(?:[^"]*"[^"]*")*[^"]*$)/
-# pat = /,(?!([^"]"[^"]")*(?![^"]"))/       # ê¨å˜
-# pat = /\.(?!([^"]*"[^"]")*(?![^"]"))/
-pat = /(?=([^\(]\(\.[^\)]\))*(?![^\(]\())\./
-pat = /(?<!\()*\./
-
-# p a.scan(pat1_1)
-# p a.scan(pat2_1)
-# puts a.split(pat1_1)
-# puts hr
-# puts a.split(pat2_1)
-# puts hr
-puts a.split(pat)
 ################################
 
-
-__END__
-
-'A.c<2.2>.d<10 12>.zero?'
-'body.body<2.4>.body<arg>.body<.7>.body'
-'You say, "Yes, I am.", "No, I am not.".'
-'You say.<Yes. I am.> . <No. I am not.>.'
-
-/.(?<![^>]*>)/
-
-A.c(2.2).d(10 12).zero?
-body.body(2.4).body(arg).body(.7).body
-You say, "Yes, I am.", "No, I am not.".
-You say.(Yes. I am.) . (No. I am not.).
