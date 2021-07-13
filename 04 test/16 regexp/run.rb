bf74fcd8c59eef023b63f19b1f42f8dc2134432c
acr002 <tamada@across-net.co.jp>
2021-07-13 16:32:24 +0900

    Add test

--- 04 test/16 regexp/run.rb
+++ 04 test/16 regexp/run.rb
@@ -1,4 +1,22 @@
 
+# text = <<BUF
+# comment
+# /* comment body
+# ******************************
+# //////////////////////////////
+# comment end */
+# BUF
+# p text
+# pat = %r{\/\*[^\*]*\*+(([^\*\/][^\*]*)\*+)*\/}
+# pat = %r{\/\*(?~\*\/)\*\/}
+# p text.match(pat)
 
+# text.match(pat) do |e|
+#   p $1
+# end
+
+text = "/* comments */"
+pat = %r{\/\*(.*?)\*\/}
+p text.match(pat)
 ################################
 
