4d205f4d08333e1ae7721c40adc1fa0727877f4b
acr002 <tamada@across-net.co.jp>
2021-08-10 16:16:47 +0900

    add hash

--- 20 files/test01.rb
+++ 20 files/test01.rb
@@ -1,4 +1,6 @@
 
+require 'pp'
+
 def search_fns(dir = Dir.pwd)
   Dir.glob(dir + '/*')
 end
@@ -6,9 +8,7 @@ end
 # ”z—ñ‚Å‚Í‚È‚­Hash‚ÅŠÇ—‚µ‚½•û‚ª’š”J‚©‚ÈH
 def check_files(dir_glob, need_file)
   h = {}
-  need_file.each do |e|
-    h[e] = 0
-  end
+  need_file.uniq.each{|e| h[e] = 0}
   dir_glob.each do |e|
     ext = File.extname(e)
     if h.has_key?(ext)
@@ -18,7 +18,29 @@ def check_files(dir_glob, need_file)
   h
 end
 
-need_file = ['.apf', '.in', '.txt', '.md', '.rb']
+def create_hash(need_file)
+  base_ext = {}
+  need_file.uniq.each do |e|
+    h = {}
+    h[:key] = e
+    h[:ext] = ".#{e}"
+    h[:count] = 0
+    case e
+    when 'in'
+      h[:base_dir] = 'data'
+    else
+      h[:base_dir] = 'fd'
+    end
+    base_ext[e] = h
+  end
+  base_ext
+end
+
+need_file = ['apf', 'in', 'txt', 'md', 'rb']
+a = create_hash(need_file)
+pp a
+
+__END__
 a = search_fns
 p check_files(a, need_file)
 
