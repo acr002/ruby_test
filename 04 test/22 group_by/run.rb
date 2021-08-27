7f4218c49212b3e4982ff58a26164b6116915339
acr002 <tamada@across-net.co.jp>
2021-08-27 10:19:10 +0900

    Add me

--- 04 test/22 group_by/run.rb
+++ 04 test/22 group_by/run.rb
@@ -1,11 +1,22 @@
 
 require 'pp'
 
-# Array#group_byの試しです。
-# ブロック内で評価結果をHashのkeyとして、値を配列で持ちます。
+# Enumerable#group_byの試しです。
+# ブロック内で評価した結果をHashのkeyとして持ち、集めた値を配列で持ちます。
 a = %w(a b c d e) + [1, 2, 3, 4, 5] + [nil, nil, nil]
 p a
-p a.sample
-b = a.group_by{_1.class}
-pp b
+# b = a.group_by{_1.class}
+# pp b
 
+# 実装
+def test_group_by(ar)
+  h = {}
+  ar.each do |e|
+    h[yield(e)] ||= []
+    h[yield(e)] << e
+  end
+  h
+end
+
+b = test_group_by(a){_1.class}
+p b
