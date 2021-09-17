f142e385d722e4631937b532cae2326ef5c5daae
maruvi01 <maruvim101@gmail.com>
2021-09-17 16:37:55 +0900

    Add Time test

---
+++ 04 test/25 date/run.rb
@@ -0,0 +1,5 @@
+
+
+p Time.now.strftime('%Y%m%d_%H%M%S')
+
+
---
+++ 04 test/25 date/runs.rb
@@ -0,0 +1,9 @@
+
+require 'date'
+
+p Time.now.strftime('%Y%m%d_%H%M%S')
+p Time.now.strftime('%c')
+p Date.today
+p Date.today.to_time
+
+
