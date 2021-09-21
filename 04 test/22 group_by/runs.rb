fa448cf41163874d465e077739cb0b77ef56c8bb
maruvi01 <maruvim101@gmail.com>
2021-09-21 15:03:30 +0900

    Add group_by, sort_by

--- 04 test/22 group_by/runs.rb
+++ 04 test/22 group_by/runs.rb
@@ -20,3 +20,31 @@ end
 
 b = test_group_by(a){_1.class}
 p b
+################################
+
+
+a = %w(a b c d e) + [1, 2, 3, 4, 5] + [nil, nil, nil]
+pp a.group_by{_1.class}
+################################
+
+a = %w(a b c d e) + [1, 2, 3, 4, 5] + [nil, nil, nil]
+# p a.group_by(&:itself).map{|k, v| [k, v.size]}.to_h.sort_by{_2}.reverse.to_h
+b = a.group_by(&:itself).map{|k, v| [k, v.size]}.sort_by{_2}.reverse.to_h
+p b.first(5).to_h
+################################
+
+a = %w(a b c d e) + [1, 2, 3, 4, 5] + [nil, nil, nil]
+# p a.group_by(&:itself).map{|k, v| [k, v.size]}.to_h.sort_by{_2}.reverse.to_h
+
+# b = a.group_by(&:itself).map{|k, v| [k, v.size]}.sort_by{_2}.reverse.to_h
+# p b.first(5).to_h
+
+# p a.group_by(&:itself).map{|k, v| [k, v.size]}.sort_by{_2}.reverse.to_h
+
+b = a.group_by(&:itself).map{|k, v| [k, v.size]}.sort_by{_2}.last(5).to_h
+p b
+################################
+
+
+
+
