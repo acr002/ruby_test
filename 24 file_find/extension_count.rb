aa5fd2bc02429eaad2cc475cb0dcd474741777c8
maruvi01 <maruvim101@gmail.com>
2021-10-18 14:49:38 +0900

    Add test

--- 24 file_find/extension_count.rb
+++ 24 file_find/extension_count.rb
@@ -3,8 +3,18 @@
 # '*'   フォルダも含めます。
 # '*.*' ファイルだけになるようです。
 fns = Dir.glob('*.*')
-fns_ext = fns.map{File.extname(_1)}.tally
-p fns, fns.size
-p fns_ext
+fns_ext = fns.map{File.extname(_1)[1..nil].to_sym}.tally
+# pp fns, fns.size
+# p fns_ext
+
+# key = :txt
+key = :rb
+re = fns_ext[key]
+if re.nil?
+  puts "#{key} is nothing"
+else
+  puts "#{key} has #{re}"
+end
+# p fns_ext.has_key?(:rb)
 ################################
 
